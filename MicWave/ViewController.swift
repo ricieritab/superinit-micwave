051b448b358859b713ebe4d257d50999a09f850b
Felipe Riceri <felipe.ricieri@theappbusiness.com>
2020-09-16 23:13:06 +0200

    First Commit

---
+++ MicWave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MicWave
+//
+//  Created by Felipe Ricieri on 16/09/2020.
+//  Copyright © 2020 Kin + Carta Create (Europe). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
